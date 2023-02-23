6fe5ea0556af727b7e059719aa2476ab2ad27c4c
Valentin <valentinlelievre2008@gmail.com>
2023-02-23 11:49:10 +0100

    🎨 Total overhaul

--- app.js
+++ app.js
@@ -3,51 +3,57 @@ const DNS_RESOLVER = 'https://dns.google/resolve?name='
 const regex = /[a-zA-Z]/;
 
 
+
+function copy_content(id) {
+  value = document.getElementById.innerHTML
+  console.log(value)
+}
+
 async function ping_time(address) {
-    let startTime = (new Date()).getTime();
-    await fetch(address)
-    let endTime = (new Date()).getTime();
-    return endTime - startTime
+  let startTime = (new Date()).getTime();
+  await fetch(address)
+  let endTime = (new Date()).getTime();
+  return endTime - startTime
 }
 
 async function resolve_dns(address) {
-    let res = await fetch(DNS_RESOLVER + address)
-    let data = await res.json()
-    return data.Answer['0'].data
+  let res = await fetch(DNS_RESOLVER + address)
+  let data = await res.json()
+  return data.Answer['0'].data
 }
 
 async function request(ip) {
-    let res = await fetch(API + ip + "/json/")
-    let data = await res.json()
-    return data
+  let res = await fetch(API + ip + "/json/")
+  let data = await res.json()
+  return data
 }
 
 async function getValue() {
-    let value = document.getElementById("ip-input").value
-    let data
+  let value = document.getElementById("ip-input").value
+  let data
 
-    if (regex.test(value)) {
-        data = await request(await resolve_dns(value))
-    } else {
-        data = await request(value)
-    }
+  if (regex.test(value)) {
+    data = await request(await resolve_dns(value))
+  } else {
+    data = await request(value)
+  }
 
-    document.getElementById("address").innerHTML = `Address : ${data.ip}`
-    document.getElementById("country").innerHTML = `Country : ${data.country_name}`
-    document.getElementById("region").innerHTML = `Region : ${data.region}`
-    document.getElementById("city").innerHTML = `City : ${data.city}`
-    document.getElementById("zip-code").innerHTML = `Zip-Code : ${data.postal}`
-    document.getElementById("calling-code").innerHTML = `Calling Code : ${data.country_calling_code}`
-    document.getElementById("currency").innerHTML = `Currency : ${data.currency_name} ( ${data.currency} )`
-    document.getElementById("org").innerHTML = "Org : " + data['org']
+  document.getElementById("address").innerHTML = `Address : ${data.ip}`
+  document.getElementById("country").innerHTML = `Country : ${data.country_name}`
+  document.getElementById("region").innerHTML = `Region : ${data.region}`
+  document.getElementById("city").innerHTML = `City : ${data.city}`
+  document.getElementById("zip-code").innerHTML = `Zip-Code : ${data.postal}`
+  document.getElementById("calling-code").innerHTML = `Calling Code : ${data.country_calling_code}`
+  document.getElementById("currency").innerHTML = `Currency : ${data.currency_name} ( ${data.currency} )`
+  document.getElementById("org").innerHTML = "Org : " + data['org']
 
-    google_map = document.getElementById("openinmaps")
-    google_map.innerHTML = "Open In Maps"
-    google_map.href = "https://www.google.com/maps/place/" + data['latitude'] + "," + data['longitude']
+  google_map = document.getElementById("openinmaps")
+  google_map.innerHTML = "Open In Maps"
+  google_map.href = "https://www.google.com/maps/place/" + data['latitude'] + "," + data['longitude']
 
-    document.getElementById("ping").innerHTML = "Ping : " + await ping_time(value) + "ms"
+  document.getElementById("ping").innerHTML = "Ping : " + await ping_time(value) + "ms"
 }
 
 (function onload() {
-    getValue()
+  getValue()
 })()
